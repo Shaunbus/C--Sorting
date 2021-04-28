561efc27616c2b8b2b5a7928756a577384bb0f6c
Shaunbus <sjbucyk@live.ca>
2021-04-28 11:11:50 -0700

    First Bubble sort implemented

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sorting
+{
+
+    public class Bubble_Sort
+    {
+        public int[] SortAsc(int[] array)
+        {
+            int temp;
+            for (int i = 0; i <= array.Length - 2; i++)
+            {
+                for (int j = 0; j <= array.Length - 2; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public int[] SortDesc(int[] array)
+        {
+            return array;
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 3, 0, 2, 5, -1, 4, 1 };
+            Bubble_Sort bubble = new Bubble_Sort();
+            bubble.SortAsc(a);
+        }
+    }
+}
